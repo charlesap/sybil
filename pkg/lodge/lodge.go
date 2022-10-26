6496a74b937844764f48280981a18b92b7f00373
Charles Perkins <chuck@kuracali.com>
2022-10-26 15:35:45 -0700

    add lowest level data structures

--- pkg/lodge/lodge.go
+++ pkg/lodge/lodge.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 )
 
+type Span struct{
+	Hsh [28] byte
+	Loc uint32
+	Lnk [8] uint32
+}
+
+type Sign [64] byte
+
+type Mdta struct{
+	Dnt uint64
+	Opr uint32
+	Acc [5] uint32
+}
+
 func Emit(name string) {
 
 	fmt.Println( name )
 }
+
