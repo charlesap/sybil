6c8525190fb9700789e2b9055adc0b086d3d6576
Charles Perkins <chuck@kuracali.com>
2022-11-12 21:46:33 -0800

    change udp port and add a poll command

--- cmd/lodge/lodge.go
+++ cmd/lodge/lodge.go
@@ -76,10 +76,44 @@ func udpResponse(udpServer net.PacketConn, addr net.Addr, buf []byte) {
 	udpServer.WriteTo([]byte(responseStr), addr)
 }
 
+func slingUDP() {
+	udpServer, err := net.ResolveUDPAddr("udp", ":1053")
+
+	if err != nil {
+		println("ResolveUDPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialUDP("udp", nil, udpServer)
+	if err != nil {
+		println("Listen failed:", err.Error())
+		os.Exit(1)
+	}
+
+	//close the connection
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("This is a UDP message"))
+	if err != nil {
+		println("Write data failed:", err.Error())
+		os.Exit(1)
+	}
+
+	// buffer to get data
+	received := make([]byte, 1024)
+	_, err = conn.Read(received)
+	if err != nil {
+		println("Read data failed:", err.Error())
+		os.Exit(1)
+	}
+
+	println(string(received))
+}
+
 func handleUDP(){
 
 	// listen to incoming udp packets
-	udpServer, err := net.ListenPacket("udp", ":1053")
+	udpServer, err := net.ListenPacket("udp", ":1969")
 	if err != nil {
 		log.Fatal(err)
 	}
