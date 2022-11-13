2246975fbf052abf294f4c865fd1ac5ab003f781
Charles Perkins <chuck@kuracali.com>
2022-11-13 11:01:24 -0800

    print the url of index requests

--- cmd/lodge/lodge.go
+++ cmd/lodge/lodge.go
@@ -180,8 +180,8 @@ var (
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	time := time.Now().String()
 	fmt.Printf(r.URL.String())
-	io.WriteString(w, htmlStart)
-	io.WriteString(w, time)
+	io.WriteString(w, htmlInfiniteStart)
+//	io.WriteString(w, time)
 	io.WriteString(w, htmlEnd)
 }
 
