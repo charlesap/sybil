642467e6edf9a8ecbd0cad15218c62986d17bd71
Charles Perkins <charlesap@gmail.com>
2022-11-20 04:51:17 +0000

    reorganize Knod structure

--- pkg/lodge/lodge.go
+++ pkg/lodge/lodge.go
@@ -39,6 +39,8 @@ const(
 	BODY0 byte = 254
 	KBNCE byte = 255
 
+
+
 //	knodSize = int(unsafe.Sizeof(Knod{}))
 //	bodySize = int(unsafe.Sizeof(Body{}))
 )
@@ -70,7 +72,7 @@ func (b Base) Init (fn string) (e error) {
 	b.Status = UNPREPARED
 
 	var k Knod
-	k,e = b.KnodByIndex(0)
+	k,e = b.KnodByIndex(Kndx{0,0,0,0,0,0})
 	if e != nil {
 		return e
 	}
@@ -105,7 +107,7 @@ func (b Base) BodyByIndex (i Kndx ) (*Body, error) {
 	return nil, nil
 }
 
-type Kdate [3]byte
+type Kdate [6]byte
 
 type Btlen [3]byte
 
@@ -113,12 +115,6 @@ type Btext [252]byte
 
 func (k Kdate) String() string {
 	return fmt.Sprintf("YYMMDD")
-}
-
-type Ktime [3]byte
-
-func (k Ktime) String() string {
-	return fmt.Sprintf("hhmmss")
 }
 
 type Hash [28] byte
@@ -135,54 +131,28 @@ func (s Sign) Archive() string {
 return t
 }
 
-type Kndx  uint32 // index of a Knod in block device of Knods
+type Slst [48] byte
+
+type Kndx  [6] byte // index of a Knod in block device of Knods
 
 type Cksm  uint32 // checksum of checksums of nodes held by this node or
                   // checksum of checksums of nodes referring to this node
 
-type Twig7 [7] Kndx
-
-func (t Twig7) String() string {
-	return fmt.Sprintf("%0.8x",t[:])
-}
-
-type Twig6 [6] Kndx
-
-func (t Twig6) String() string {
-	return fmt.Sprintf("%0.8x",t[:])
-}
-
-type Span7 struct{ // 56 bytes 
-	Hsh Hash
-	Lnk Twig7
-}
-
-func (s Span7) Archive() string {
-	return s.Hsh.Archive()
-}
-
-type Span6 struct{ // 56 bytes 
-	Hsh Hash
-	Top Kndx
-	Lnk Twig6
-}
-
-func (s Span6) Archive() string {
-	return s.Hsh.Archive()
-}
 
 type Knod struct{ // 256 bytes // knowledge node, Tnod is a text representation
 	Op   byte  // op except 0 = hash slot free, 255 = hash slot available due to allocation bounce on content size
-	Date Kdate
-	Tag  Kndx  // index to a well-known universal label for un-tracked tagging, expanded to hash on transmission or zero, also may be used as salt
-	H    Span7
 	Idpt byte  // principal signer depth, 1 = self
-	Time Ktime
-	Iloc Kndx  // principal signer quick link
-	P    Span6
-	Pchk Cksm  // checksum of Pchk of nodes held... if two lodges differ on this, synchronization is indicated
-	Rchk Cksm  // checksum of Rchk of nodes held... if two lodges differ on this, synchronization is indicated
-	R    Span6
+	Date Kdate
+	Hk   Hash
+	Hr   Hash
+	Pchk Cksm  // checksum of Rchk of nodes held... if two lodges differ on this, synchronization is indicated
+	Itag Kndx  // index to a well-known universal label for un-tracked tagging, expanded to hash on transmission or zero, also may be used as salt
+	Ptag Kndx
+	Tp   Slst
+	Rchk Cksm  // checksum of Pchk of nodes held... if two lodges differ on this, synchronization is indicated
+	Ttag Kndx 
+	Rtag Kndx
+	Tr   Slst
 	S    Sign
 }
 
@@ -217,20 +187,21 @@ func Op2string(o byte) string {
 }
 
 func (k Knod) UnixTime() time.Time {
-	return time.Unix( int64(k.Time[0])+
-	                  int64(k.Time[1])*256+
-	                  int64(k.Time[2])*65536+
-	                  int64(k.Date[0])*16777216+
-	                  int64(k.Date[1])*4294967296+
-	                  int64(k.Date[2])*1099511627776,0)
+	return time.Unix( int64(k.Date[0])+
+	                  int64(k.Date[1])*256+
+	                  int64(k.Date[2])*65536+
+	                  int64(k.Date[3])*16777216+
+	                  int64(k.Date[4])*4294967296+
+	                  int64(k.Date[5])*1099511627776,0)
 }
 
 func (k Knod) Archive() string {
-	h:= Hash {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	hs:= Hash {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	hp:= Hash {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 	t:=k.UnixTime()
 
 	//255 character string output so the 256th may be a newline 
-	return fmt.Sprintf("%v %v%s%s%s%s%s", Op2string(k.Op), time.Unix(t.UnixMilli(),0), h.Archive() , k.H.Archive(), k.P.Archive(), k.R.Archive(), k.S.Archive()) 
+	return fmt.Sprintf("%v %v%s%s%s%s%s", Op2string(k.Op), time.Unix(t.UnixMilli(),0), k.Hk.Archive() , k.Hr.Archive(), hs.Archive(), hp.Archive(), k.S.Archive()) 
 }
 
 
@@ -291,18 +262,20 @@ func ZeroBody () Body {
 
 func ZeroKnod () Knod {
 
-	return Knod { 
-	0,
-	Kdate {0,0,0},
-	0,
-	Span7 {Hash {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, Twig7 {0,0,0,0,0,0,0}},
-	0,
-	Ktime {0,0,0},
-	0,
-	Span6 {Hash {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},0, Twig6 {0,0,0,0,0,0}},
+	return Knod {
 	0,
 	0,
-	Span6 {Hash {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},0, Twig6 {0,0,0,0,0,0}},
+	Kdate {0,0,0,0,0,0},
+	Hash {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	Hash {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	0,
+	Kndx {0,0,0,0,0,0},
+	Kndx {0,0,0,0,0,0},
+	Slst {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	0,
+	Kndx {0,0,0,0,0,0},
+	Kndx {0,0,0,0,0,0},
+	Slst {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 	Sign {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}}
 
 }
